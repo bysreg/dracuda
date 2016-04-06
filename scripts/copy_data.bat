e7af94766861d5130fe42d30f2a24274baa44a31
hilman <hilmanbeyri@gmail.com>
2016-04-05 21:29:34 -0400

    Add build/.gitignore and add script to copy some data stuff to inside the build folder

---
+++ scripts/copy_data.bat
@@ -0,0 +1,17 @@
+mkdir ..\build\p3\cubemaps\stpet
+mkdir ..\build\p3\cubemaps\teide
+mkdir ..\build\p3\custom_scenes
+mkdir ..\build\p3\extra
+mkdir ..\build\p3\images
+mkdir ..\build\p3\models
+mkdir ..\build\p3\scenes
+mkdir ..\build\p3\stylesheets
+
+copy ..\cubemaps\stpet\*.* ..\build\p3\cubemaps\stpet
+copy ..\cubemaps\teide\*.* ..\build\p3\cubemaps\teide
+copy ..\custom_scenes\*.* ..\build\p3\custom_scenes
+copy ..\extra\*.* ..\build\p3\extra
+copy ..\images\*.* ..\build\p3\images
+copy ..\models\*.* ..\build\p3\models
+copy ..\scenes\*.* ..\build\p3\scenes
+copy ..\stylesheets\*.* ..\build\p3\stylesheets
